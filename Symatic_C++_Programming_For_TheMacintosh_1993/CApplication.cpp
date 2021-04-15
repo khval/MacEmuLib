aa734f53b1fdf465702408fbe4a5205d2a8c8743
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-04-15 11:22:16 +0200

    Heavy debugging of CApplication, method Run.

--- Symatic_C++_Programming_For_TheMacintosh_1993/CApplication.cpp
+++ Symatic_C++_Programming_For_TheMacintosh_1993/CApplication.cpp
@@ -15,53 +15,70 @@ void CApplication::CreateDocument()
 
 CApplication::CApplication()
 {
-	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
-	cbureaucrat = NULL;
+	printf("%s:%s:%d - this: %08x\n",__FILE__,__FUNCTION__,__LINE__, this);
+	nextApp = NULL;
 }
 
 void CApplication::Exit()
 {
-	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
+	printf("%s:%s:%d - this: %08x\n",__FILE__,__FUNCTION__,__LINE__, this);
 }
 
 void CApplication::Run()
 {
+	CApplication *currentApp;
 	bool quit = false;
 
-	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
+	printf("%s:%s:%d - this: %08x\n",__FILE__,__FUNCTION__,__LINE__, this);
 
+	this->SetUpFileParameters();
 	this->CreateDocument();
 
-	if (cbureaucrat)
+	printf("found a bureaucrat...\n");
+
+	int maxSleep;
+	bool hasWindow;
+
+	while( ! quit )
 	{
-		printf("found a bureaucrat...\n");
+		hasWindow = false;
 
-		if (cbureaucrat -> itsWindow)	// class
+		for (currentApp = this ; currentApp; currentApp = currentApp -> nextApp)
 		{
-			printf("he has a window\n");
+			printf("Running App: %08x\n",currentApp);
+			printf("currentApp -> itsWindow: %08x - offsetof %d\n",currentApp -> itsWindow,offsetof(CApplication,itsWindow) );
+			printf("currentApp -> itsGopher: %08x - offsetof %d\n",currentApp -> itsGopher,offsetof(CApplication,itsGopher));
 
-			SetPort( cbureaucrat -> itsWindow -> window );
-
-			if (cbureaucrat -> itsGopher)
+			if (currentApp -> itsWindow)	// class
 			{
-				int maxSleep;
+				hasWindow = true;
+				SetPort( currentApp -> itsWindow -> window );
 
-				printf("he has work to do\n");
+				quit = n(handelEvensts)( currentApp -> itsWindow -> window);
 
-				while( ! quit )
+				if (currentApp -> itsGopher)
 				{
-					quit = n(handelEvensts)( cbureaucrat -> itsWindow -> window);
-
-					cbureaucrat -> itsGopher -> Dawdle(&maxSleep);
-					Delay(maxSleep);
+					currentApp -> itsGopher -> Dawdle(&maxSleep);
 				}
+				else quit = true;
+
+				printf("Delay: %d\n",maxSleep);
+				Delay(maxSleep);
+
+				if (quit) break;
 			}
+		}
+
+		if (hasWindow == false)
+		{
+			printf("no window, has to quit\n");
+			quit = true;
 		}
 	}
 }
 
 void CApplication::SetUpFileParameters()
 {
-	printf("%s:%s:%d\n",__FILE__,__FUNCTION__,__LINE__);
+	printf("%s:%s:%d - this: %08x\n",__FILE__,__FUNCTION__,__LINE__, this);
 }
 
